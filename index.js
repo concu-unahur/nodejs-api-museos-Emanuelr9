1887fd33f13ca96670f1178fff6a30654de812f2
Emanuelr9 <emanuelr9@yahoo.com>
2020-03-10 20:42:24 -0300

    Falta el for

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const superagent = require('superagent');
+const fs = require('fs');
 
 function imprimirMuseos(error, respuesta) {
   if (error) {
@@ -17,6 +18,17 @@ console.log('Antes de llamar a superagent')
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .end(escibirArchivo)
+
+ function escibirArchivo(err, res) {
+  const museos = res.body.results;  
+  fs.writeFile('museos.txt',museos[3].nombre,terminar)
+
+ }
+
+
+ function terminar(err){
+   console.log("Fin")
+ }
 
 console.log('Después de llamar a superagent')
